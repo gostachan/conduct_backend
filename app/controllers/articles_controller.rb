f766e65e04fad66f6be57cfbe288f42d7b24e081
Hara Misaki <m.kevavu05@gmail.com>
2024-02-13 13:18:38 +0900

    create controller, create and destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,23 @@ class ArticlesController < ApplicationController
     @articles = Article.includes(:user).order(created_at: :desc).limit(10)
     render json: @articles.as_json(include: {user: {only: :name}}, except: :user_id)
   end
+
+  def create 
+    current_user_id = 1
+    @user = User.find(current_user_id)
+    @article = @user.articles.create(article_params)
+    render json: @article
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+  end
+
+
+  private 
+
+  def article_params 
+    params.require(:article).permit(:title, :description, :content)
+  end
 end
